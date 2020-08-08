bdcd83aa2bd0f65aa0c298ce1faf80c12c0ae829
Shivanand <mpshivanandpatil2002@gmail.com>
2020-08-08 16:53:09 +0530

    Initial Commit

---
+++ data_structures_intro.c
@@ -0,0 +1,5 @@
+// Data: the quantities, character or symbols on which operations are preformed by a computer, which may be stored and transmited in the form of electrical signals and recorded on magnetic, optical or mechanical recording media
+
+// Information: if data is arranged in a systematic and understandeable way it becomes information
+
+// Data Structure: systematic way to organise data so that it can be used efficiently
