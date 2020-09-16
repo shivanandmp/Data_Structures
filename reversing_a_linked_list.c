430c9bb69eda7f3338e2a83d61520fe308faacb8
Shivanand <mpshivanandpatil2002@gmail.com>
2020-09-16 16:10:47 +0530

    Reversed a Linked list

---
+++ reversing_a_linked_list.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *link;
+};
+
+void add_at_end(struct node* *ptr, int data) { // this function adds without traversing the whole list , so it is faster than the previous one
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+
+	temp->data = data;
+	temp->link = NULL;
+
+	(*ptr)->link = temp;
+	*ptr = temp;
+}
+
+void print_data(struct node *head) {
+    struct node *ptr = head;
+    while(ptr != NULL) {
+        printf("%d ",ptr->data);
+        ptr = ptr->link;
+    }
+    printf("\n");
+}
+
+void rev_list(struct node **head) {
+	struct node *prev = NULL;
+	struct node *next = NULL;
+
+	while(*head != NULL) {
+		next = (*head)->link;
+		(*head)->link = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+}
+
+void del_list(struct node **head) {
+	struct node *temp = *head;
+
+	while(*head != NULL){
+		temp = temp->link;
+		free(*head);
+		*head = temp;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	struct node *head = malloc(sizeof(struct node));
+	head->data = 62;
+	head->link = NULL;
+	struct node* ptr = head;
+
+	add_at_end(&ptr,63);
+	add_at_end(&ptr,64);
+	add_at_end(&ptr,65);
+	add_at_end(&ptr,67);
+	add_at_end(&ptr,69);
+	print_data(head);
+
+	rev_list(&head);
+	print_data(head);
+
+	del_list(&head);
+
+	if(head == NULL){
+		printf("Linked List Is Emptied\n");
+	} else{
+		printf("Program Fucked Up\n");
+	}
+	return 0;
+}
